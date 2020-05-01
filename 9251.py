77c84b66d824edad8168a87aaebcf8ea0b07fe22
journeytorainbow <58691899+journeytorainbow@users.noreply.github.com>
2020-05-01 20:29:56 +0900

    9251번

---
+++ 9251.py
@@ -0,0 +1,19 @@
+import sys 
+
+# col
+A = sys.stdin.readline().rstrip()
+# row
+B = sys.stdin.readline().rstrip()
+
+array = [[0]*(len(A)+1) for _ in range(len(B)+1)]
+
+for i in range(0, len(B)):
+    for j in range(0, len(A)):
+        if A[j] == B[i]:
+            array[i+1][j+1] = array[i][j] + 1
+        else:
+            array[i+1][j+1] = max(array[i][j+1], array[i+1][j])
+
+print(array[len(B)][len(A)])
+
+    
--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 
 <li><a href="https://www.acmicpc.net/problem/11053">11053번 : 가장 긴 증가하는 부분 수열(LIS) </a></li>
 
+<li><a href="https://www.acmicpc.net/problem/9251">9251번 : LCS </a></li>
+
